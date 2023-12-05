53017fd0e63b0c42675d74b0d87871b501fe65a3
henrvend <henrik.vendel@outlook.com>
2023-12-05 09:40:15 +0100

    försöker kalla på balance i add

--- src/avl.py
+++ src/avl.py
@@ -17,6 +17,24 @@ class AVL(bst.BST):
             self.cons(AVL(), AVL())
 
     def add(self, v):
+
+
+        if self.is_empty():
+            self.__init__(value=v)
+            return self
+        if self.get_value==v:
+            return self
+        if v < self.get_value():
+            self.cons(self.get_lc().add(v), self.get_rc())
+            self.balance()
+            return self
+        if v > self.get_value():
+            self.cons(self.get_lc(), self.get_rc().add(v))
+            self.balance()
+            return self
+        
+
+    
         '''
         Example which shows how to override and call parent methods.  You
         may remove this function and overide something else if you'd like.
@@ -30,6 +48,14 @@ class AVL(bst.BST):
         AVL-balances around the node rooted at `self`.  In other words, this
         method applies one of the following if necessary: slr, srr, dlr, drr.
         '''
+
+
+        if ((self.get_lc().height()-self.get_rc().height())==2):
+            self.balance()
+        elif((self.get_lc().height()-self.get_rc().height())==-2):
+            self.balance()
+
+
         log.info("TODO@src/avl.py: implement balance()")
         self.slr().srr().dlr().drr() # TODO: apply these methods correctly
         return self
@@ -61,6 +87,20 @@ class AVL(bst.BST):
         '''
         log.info("TODO@src/avl.py: implement drr()")
         return self
+    
+
+    def height(self):
+        '''
+        Returns the height of the tree.
+        '''
+        if self.is_empty():
+            return 0
+        elif self.get_lc().height() > self.get_rc().height():
+            return 1 + self.get_lc().height()
+        elif self.get_rc().height() >= self.get_lc().height():
+            return 1 + self.get_rc().height()
+        
+
 
 if __name__ == "__main__":
     log.critical("module contains no main module")
